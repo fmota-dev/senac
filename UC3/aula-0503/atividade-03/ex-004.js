84f61c30f48c03f0e64f189478f49132e5205348
Filipe Mota <126270931+fmota-dev@users.noreply.github.com>
2024-03-06 18:28:00 -0300

    aula-05/03

--- UC3/aula-0503/atividade-03/ex-004.js
+++ UC3/aula-0503/atividade-03/ex-004.js
@@ -1,7 +1,6 @@
 // GERAL
 
 const paragrafosGeral = document.getElementsByTagName("p");
-
 const resultadoGeral = document.getElementById("resultadoGeral");
 resultadoGeral.innerText = "Resultado geral " + paragrafosGeral.length;
 
@@ -9,8 +8,5 @@ resultadoGeral.innerText = "Resultado geral " + paragrafosGeral.length;
 // ARTICLES
 
 const paragrafosArticle = document.querySelectorAll("article p");
-
 const resultadoArticle = document.getElementById("resultadoArticle");
 resultadoArticle.innerText = "Resultado article " + paragrafosArticle.length;
-
-
