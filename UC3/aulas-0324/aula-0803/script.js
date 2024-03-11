58f802007aa0c937340dce798d4b12c6082b81d4
@fmota-dev <fmota.web@gmail.com>
2024-03-11 11:16:57 -0300

    calculadora-v1.0

--- UC3/aulas-0324/aula-0803/script.js
+++ UC3/aulas-0324/aula-0803/script.js
@@ -1,44 +1,80 @@
+let calculoFeito = false;
+let operadores = ['+', '-', 'x', '/'];
+
+// Função para inserir um valor na tela da calculadora
 function inserir(valor) {
-  let resultado = document.getElementById("resultado").innerText;
+	const displayElement = document.getElementById('display');
+	let display = displayElement.innerText;
+	const ultimoCaractere = display[display.length - 1];
 
-  // Verifica se o resultado atual é igual a 0 e o valor inserido não é um operador
-  if (resultado === "0" && !["+", "-", "x", "/"].includes(valor)) {
-    resultado = "";
-  }
-
-  // Verifica se o último caractere é um operador
-  if (
-    ["+", "-", "x", "/"].includes(resultado.slice(resultado.length - 1)) &&
-    ["+", "-", "x", "/"].includes(valor)
-  ) {
-    // Remove o último operador antes de inserir o novo
-    resultado = resultado.slice(0, resultado.length - 1);
-  }
-
-  document.getElementById("resultado").innerText = resultado + valor;
+	// Verifica se o cálculo já foi feito e o valor inserido não é um operador ou é um ponto decimal
+	if (calculoFeito && (!operadores.includes(valor) || valor === '.')) { 
+		displayElement.innerText = valor;
+		calculoFeito = false;
+	} 
+	// Verifica se o cálculo já foi feito e o valor inserido é um operador
+	else if (calculoFeito && operadores.includes(valor)) {
+		displayElement.innerText += valor;
+		calculoFeito = false;
+	} 
+	// Verifica se o valor inserido e o último caractere são operadores
+	else if (
+		operadores.includes(valor) &&
+		operadores.includes(ultimoCaractere)
+	) {
+		displayElement.innerText = display.slice(0, -1) + valor;
+	} 
+	// Verifica se o valor inserido é um ponto decimal e o último caractere também é um ponto decimal
+	else if (valor === '.' && ultimoCaractere === '.') {
+		return;
+	} 
+	// Caso contrário, apenas adiciona o valor ao display
+	else {
+		displayElement.innerText += valor;
+	}
 }
+
+let btnsInserir = document.querySelectorAll('.inserir');
+btnsInserir.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		inserir(btn.innerText);
+	});
+});
 
 // Função para limpar o conteúdo da tela da calculadora
 function limpar() {
-  document.getElementById("resultado").innerText = "";
+	document.getElementById('display').innerText = '';
 }
+
+let btnLimpar = document.getElementById('limpar');
+btnLimpar.addEventListener('click', limpar);
 
 // Função para apagar o último caractere na tela da calculadora
 function apagar() {
-  let resultado = document.getElementById("resultado").innerText;
-  resultado = document.getElementById("resultado").innerText =
-    resultado.substring(0, resultado.length - 1);
+	let displayElement = document.getElementById('display');
+	let display = displayElement.innerText;
+	display = display.substring(0, display.length - 1);
+	displayElement.innerText = display;
 }
 
-// Função para calcular o resultado da expressão na tela da calculadora
+let btnApagar = document.getElementById('apagar');
+btnApagar.addEventListener('click', apagar);
+
+// Função para calcular o display da expressão na tela da calculadora
 function calcular() {
-  let resultado = document.getElementById("resultado").innerText;
-
-  if (resultado !== "") {
-    let expressao = resultado.replaceAll("x", "*");
-    let resultadoFinal = eval(expressao);
-    document.getElementById("resultado").innerText = resultadoFinal;
-  }
+	let displayElement = document.getElementById('display');
+	let display = displayElement.innerText; // Definindo a variável display dentro da função calcular
+	if (display) {
+		let expressao = display.replaceAll('x', '*');
+		try {
+			let displayFinal = eval(expressao);
+			displayElement.innerText = displayFinal;
+			calculoFeito = true;
+		} catch (erro) {
+			displayElement.innerText = 'Expressão inválida';
+		}
+	}
 }
 
-
+let btnCalcular = document.getElementById('calcular');
+btnCalcular.addEventListener('click', calcular);
