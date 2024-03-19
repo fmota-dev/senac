42616861269c2371ac93ab50060fbdbf925e651e
@fmota-dev <fmota.web@gmail.com>
2024-03-19 00:51:43 -0300

    tela-login com localstorage

---
+++ UC3/aulas-0224/aula-2902/assets/js/login.js
@@ -0,0 +1,54 @@
+let usuarioLogin = document.getElementById('usuario-login');
+let senhaLogin = document.getElementById('senha-login');
+const botaoLogin = document.getElementById('btn-login');
+let usuariosCadastrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+function realizarLogin(event) {
+	event.preventDefault();
+	const usuario = usuarioLogin.value;
+	const senha = senhaLogin.value;
+
+	if (usuario && senha) {
+		const usuarioCadastrado = usuariosCadastrados.find(
+			(usuarioCadastrado) =>
+				usuarioCadastrado.usuario === usuario &&
+				usuarioCadastrado.senha === senha
+		);
+
+		if (usuarioCadastrado) {
+			abrirModal('Login realizado com sucesso! redirecionando...');
+			// esperar um tempo até redirecionar
+			setTimeout(() => {
+				window.open(
+					'https://fmota-dev.github.io/controle-financeiro/',
+					'_blank'
+				);
+				fecharModal();
+			}, 2000);
+			usuarioLogin.value = '';
+			senhaLogin.value = '';
+		} else {
+			abrirModal('Usuário ou senha incorretos!');
+			let botaoFechar = document.querySelector('.close');
+			botaoFechar.addEventListener('click', fecharModal);
+		}
+	} else {
+		abrirModal('Preencha todos os campos!');
+		let botaoFechar = document.querySelector('.close');
+		botaoFechar.addEventListener('click', fecharModal);
+	}
+}
+
+function abrirModal(texto) {
+	let modal = document.querySelector('.modal');
+	modal.classList.add('ativo');
+	let textoModal = document.querySelector('#modal-text');
+	textoModal.textContent = texto;
+}
+
+function fecharModal() {
+	let modal = document.querySelector('.modal');
+	modal.classList.remove('ativo');
+}
+
+botaoLogin.addEventListener('click', realizarLogin);
